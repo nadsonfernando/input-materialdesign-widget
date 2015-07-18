8b895b825cfef85471512c88d073e80a6e9f0791
Douglas Hennrich <Douglas_b_h@hotmail.com>
2015-07-18 04:47:58 -0300

    Add Example

---
+++ example/controllers/index.js
@@ -0,0 +1,11 @@
+// On return Key from firstTextField
+$.firstTextField.addEventListener( 'return', function(e){
+	$.secondTextField.focus();
+});
+
+// On blur Key from secondTextField
+$.secondTextField.listener( 'blur', function(e){
+	alert( 'You just Blur secondTextField' );
+});
+
+$.win.open();
